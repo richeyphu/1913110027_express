ed1877e4747c2f76e68a11f5abcb0b6cf2749f8f
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-12-10 13:58:50 +0700

    Get All Staffs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const companyRouter = require("./routes/company");
+const staffRouter = require("./routes/staff");
 
 const app = express();
 
@@ -24,5 +25,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/company", companyRouter);
+app.use("/staff", staffRouter);
 
 module.exports = app;
---
+++ models/staff.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const staffSchema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    salary: { type: Number },
+    created: {type: Date, default: Date.now},
+  },
+  { collection: "staffs" }
+);
+
+const staff = mongoose.model("Staffs", staffSchema);
+
+module.exports = staff;
