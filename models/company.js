4f0f3d66960166e3bc65e21b277a33b0574dc113
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-12-10 11:44:23 +0700

    Workshop A

--- models/company.js
+++ models/company.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const companySchema = new Schema({
-  name: String, // String is shorthand for {type: String}
-  address: {
-    province: String,
+const companySchema = new Schema(
+  {
+    name: String, // String is shorthand for {type: String}
+    address: {
+      province: String,
+    },
   },
-});
+  { collection: "settings" }
+);
 
 const company = mongoose.model("Companies", companySchema);
 
