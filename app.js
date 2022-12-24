3506f819b4718bd0c0b92d470f65aa3492a0560e
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-12-24 09:50:30 +0700

    Workshop A

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const companyRouter = require("./routes/company");
 const staffRouter = require("./routes/staff");
+const shopRouter = require("./routes/shop");
 
 const app = express();
 
@@ -26,5 +27,6 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/company", companyRouter);
 app.use("/staff", staffRouter);
+app.use("/shop", shopRouter);
 
 module.exports = app;
---
+++ controllers/shopController.js
@@ -0,0 +1,9 @@
+const Shop = require("../models/shop");
+
+exports.index = async (req, res, next) => {
+  const shop = await Shop.find().sort({ _id: -1 });
+
+  res.status(200).json({
+    data: shop,
+  });
+};
